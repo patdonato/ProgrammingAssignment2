bf8e03e4eb36ebcde6f3dd622a668dbeca106c55
patdonato <prbdonato@gmail.com>
2018-05-14 19:51:35 +0800

    with comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,3 +1,9 @@
+## The following functions allow user to get the inverse of their input matrix.
+## However, if the inverse is already stored, it gets the inverse from the cache to save time and memory.
+
+## makeCacheMatrix creates a "special matrix", but it just ultimately returns a list of functions when ran.
+## When a new matrix is inputted, makeCacheMatrix reinitializes all the values needed in cacheSolve.
+
 makeCacheMatrix <- function(mat = matrix()) {
     i <- NULL
     set <- function(y) {
@@ -10,6 +16,9 @@ makeCacheMatrix <- function(mat = matrix()) {
     list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
+
+## cacheSolve will get the inverse of the "special matrix" created in makeCacheMatrix.
+
 cacheSolve <- function(mat, ...) {
     i <- mat$getinv()
     if(!is.null(i)) {
@@ -21,3 +30,8 @@ cacheSolve <- function(mat, ...) {
     mat$setinv(i)
     i
 }
+
+## Try:
+## x <- makeCacheMatrix(diag(5))
+## cacheSolve(x)
+## cacheSolve(x) will return the message "getting cached data" when ran the second time.
